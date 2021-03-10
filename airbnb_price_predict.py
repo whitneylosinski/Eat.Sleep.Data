e273e8ec6c94526327e9532be5c10c1a7a39626d
Alex Felice <golfzander@comcast.net>
2021-03-10 17:04:22 -0600

    Updated pickle file and html for latest features

--- airbnb_price_predict.py
+++ airbnb_price_predict.py
@@ -2,25 +2,32 @@ import pandas as pd
 import tensorflow as tf
 import numpy as np
 
-def model_loss_accuracy():
-    # Import the model to a new object
-    nn_imported = tf.keras.models.load_model('PricePredictionModel.h5')
+# def model_loss_accuracy():
+#     # Import the model to a new object
+#     nn_imported = tf.keras.models.load_model('PricePredictionModel.h5')
 
-    # Import test data
-    X_test_scaled = np.genfromtxt('X_test_scaled.csv', delimiter=',')
-    y_test = np.genfromtxt('y_test.csv', delimiter=',')
+#     # Import test data
+#     X_test_scaled = np.genfromtxt('X_test_scaled.csv', delimiter=',')
+#     y_test = np.genfromtxt('y_test.csv', delimiter=',')
 
-    # Evaluate the model using the test data
-    model_loss, model_accuracy = nn_imported.evaluate(X_test_scaled,y_test,verbose=2)
-    results = f"Loss: {model_loss}, Accuracy: {model_accuracy}"
-    return results
+#     # Evaluate the model using the test data
+#     model_loss, model_accuracy = nn_imported.evaluate(X_test_scaled,y_test,verbose=2)
+#     results = f"Loss: {model_loss}, Accuracy: {model_accuracy}"
+#     return results
 
-def predict(district,accommodates, bedrooms, baths, host_listings_count,security_deposit,cleaning_fee,reviews_ltm,host_days,essentials_chk):
-    # Clean inputs from webpage
-    # ['host_listings_count', 'accommodates', 'bathrooms', 'bedrooms',
-    #    'security_deposit', 'cleaning_fee', 'number_of_reviews_ltm',
-    #    'reviews_per_month', 'days_host', 'essentials',
-    #    'neighbourhood_cleansed_District 19']
+def predict(district,accommodates, bedrooms, baths, host_listings_count,security_deposit,cleaning_fee,reviews_per_month,number_of_reviews,guests_included):
+    # Final Features:
+    # 1. 'bathrooms',
+    # 2. 'cleaning_fee',
+    # 3. 'accommodates',
+    # 4. 'host_listings_count',
+    # 5. 'bedrooms',
+    # 6. 'reviews_per_month',
+    # 7. 'neightborhood_cleansed_District 19'
+    # 8. 'security_deposit'
+    # 9. 'number_of_reviews'
+    # 10. 'guests_included'
+
     # Test opening saved model and run prediction
     import pickle
     filename = 'rfr_model_post_feat_sel_2.pickle'
@@ -36,38 +43,25 @@ def predict(district,accommodates, bedrooms, baths, host_listings_count,security
     if (host_listings_count==0):
         host_listings_count=1
 
-    if (host_days==0):
-        host_days=2
-    
-    if (essentials_chk == 'essentials'):
-        essentials = 1
-    else:
-        essentials = 0
-    #  host_listings_count = 76
-    # security_deposit = 183
-    # cleaning_fee = 96
-    # number_of_reviews_ltm = 23
-    reviews_per_month = int(reviews_ltm)/12
-    # days_host = 1199
-    # essentials = 1
-    # neighbourhood_cleansed_District_19 = 0
+    if (guests_included==0):
+        guests_included=1
     # Aggregate test data and reshape
-    X_test = [host_listings_count,accommodates,baths,bedrooms,security_deposit,cleaning_fee,reviews_ltm,reviews_per_month,host_days,essentials,neighbourhood_cleansed_District_19]
+    X_test = [host_listings_count, accommodates, baths, bedrooms,security_deposit, cleaning_fee, guests_included,number_of_reviews, reviews_per_month,neighbourhood_cleansed_District_19]
     X_test = np.array(X_test)
     X_test = X_test.reshape(1,-1)
     # Make predictions using the test data
     result = pickle_model.predict(X_test)
     return int(round(result[0],0))
 
-def addition(input1, input2):
-    result = input1 + input2
-    return result
+# def addition(input1, input2):
+#     result = input1 + input2
+#     return result
 
-def summarize_inputs(districts, property_type, room_type, accommodates, bedrooms, beds, baths):
-    # result = f"Address:{address}\nNeighborhood: {neighborhood}\nProperty Type: {property_type}\nRoom Type: {room_type}\nBedrooms: {bedrooms}\nBeds: {beds}\nBaths: {baths}"
-    result = pd.DataFrame(  
-                        data=[districts, property_type, room_type, bedrooms, beds, baths], 
-                        index=['Districts', 'Property Type', 'Room Type',  'Bedrooms', 'Beds', 'Baths'],
-                        columns=["Inputs"]
-                        )                  
-    return result
+# def summarize_inputs(districts, property_type, room_type, accommodates, bedrooms, beds, baths):
+#     # result = f"Address:{address}\nNeighborhood: {neighborhood}\nProperty Type: {property_type}\nRoom Type: {room_type}\nBedrooms: {bedrooms}\nBeds: {beds}\nBaths: {baths}"
+#     result = pd.DataFrame(  
+#                         data=[districts, property_type, room_type, bedrooms, beds, baths], 
+#                         index=['Districts', 'Property Type', 'Room Type',  'Bedrooms', 'Beds', 'Baths'],
+#                         columns=["Inputs"]
+#                         )                  
+#     return result
--- app.py
+++ app.py
@@ -8,7 +8,19 @@ app = Flask(__name__)
 # Default route
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        district = 1,
+        accommodates = 1,
+        bedrooms = 1,
+        baths = 1,
+        host_listings_count = 0,
+        security_deposit = 100,
+        cleaning_fee = 50,
+        reviews_per_month = 0,
+        number_of_reviews = 0,
+        guests_included = 1
+        )
 
 
 @app.route('/', methods=['POST'])
@@ -17,18 +29,18 @@ def my_form_post():
     # my_input2 = request.form['input2']
     # input1 = int(my_input1)
     # input2 = int(my_input2)
-    district = request.form['selDistrict']
-    accommodates = request.form['selAccommodates']
-    bedrooms = request.form['selBedrooms']
-    baths = request.form['selBaths']
-    host_listings_count = request.form['host_listings_count']
-    security_deposit = request.form['security_deposit']
-    cleaning_fee = request.form['cleaning_fee']
-    reviews_ltm = request.form['reviews_ltm']
-    host_days = request.form['host_days']
-    essentials_chk = request.form['essentials_chk']
+    district = int(request.form['selDistrict'])
+    accommodates = int(request.form['selAccommodates'])
+    bedrooms = int(request.form['selBedrooms'])
+    baths = int(request.form['selBaths'])
+    host_listings_count = int(request.form['host_listings_count'])
+    security_deposit = int(request.form['security_deposit'])
+    cleaning_fee = int(request.form['cleaning_fee'])
+    reviews_per_month = int(request.form['reviews_per_month'])
+    number_of_reviews = int(request.form['number_of_reviews'])
+    guests_included = int(request.form['guests_included'])
     # result = airbnb_price_predict.addition(input1, input2)
-    result = airbnb_price_predict.predict(district,accommodates, bedrooms, baths, host_listings_count,security_deposit,cleaning_fee,reviews_ltm,host_days,essentials_chk)
+    result = airbnb_price_predict.predict(district,accommodates, bedrooms, baths, host_listings_count,security_deposit,cleaning_fee,reviews_per_month,number_of_reviews,guests_included)
     return render_template(
         "index.html",
         result = result,
@@ -39,9 +51,9 @@ def my_form_post():
         host_listings_count = host_listings_count,
         security_deposit = security_deposit,
         cleaning_fee = cleaning_fee,
-        reviews_ltm = reviews_ltm,
-        host_days = host_days,
-        essentials_chk = essentials_chk,
+        reviews_per_month = reviews_per_month,
+        number_of_reviews = number_of_reviews,
+        guests_included = guests_included
     )
 
 if __name__ == "__main__":
